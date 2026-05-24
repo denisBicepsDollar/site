808032af81c39e7ddfbccb462c483634ce77dd79
denisBiceps <examp@le.ru>
2026-05-24 04:25:42 +0300

    нормалды работая

--- back/server/src/config/index.js
+++ back/server/src/config/index.js
@@ -4,14 +4,12 @@ export default {
     port: process.env.PORT || configJson.main.port || 3000,
 
     db: {
-        // если есть DATABASE_URL (Alwaysdata) — используем её
         connectionString: process.env.DATABASE_URL || null,
-
-        // fallback для локальной разработки
-        host: process.env.DB_HOST || configJson.db.host,
-        port: process.env.DB_PORT || configJson.db.port,
-        user: process.env.DB_USER || configJson.db.user,
-        password: process.env.DB_PASSWORD || configJson.db.password,
-        database: process.env.DB_NAME || configJson.db.defaultDatabase,
+        host:            process.env.DB_HOST     || configJson.db.host,
+        port:            process.env.DB_PORT     || configJson.db.port,
+        user:            process.env.DB_USER     || configJson.db.user,
+        password:        process.env.DB_PASSWORD || configJson.db.password,
+        database:        process.env.DB_NAME     || configJson.db.defaultDatabase,
+        reportsDatabase: process.env.DB_REPORTS  || configJson.db.reportsDatabase,
     }
 };
