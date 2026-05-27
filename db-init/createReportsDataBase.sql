f5a8336ff5010fdf93bbe31d83e62a424747e200
denisBiceps <examp@le.ru>
2026-05-27 17:04:54 +0300

    Добавил воркер для отчетов

--- db-init/createReportsDataBase.sql
+++ db-init/createReportsDataBase.sql
@@ -12,11 +12,19 @@ CREATE TABLE IF NOT EXISTS reports (
     config        JSONB,
     status        TEXT DEFAULT 'В ожидании...',
     result_path   TEXT,
+    filter        JSONB,
     mime          TEXT,
     error         TEXT,
     source_db_id  TEXT,
     created_at    TIMESTAMPTZ DEFAULT now(),
-    finished_at   TIMESTAMPTZ
+    finished_at   TIMESTAMPTZ,
+    filter      JSONB,
+    columns     JSONB,
+    count       TEXT,
+    avg         TEXT,
+    group_by    TEXT,
+    order_by    TEXT,
+    order_dir   TEXT,
     );
 
 CREATE INDEX IF NOT EXISTS reports_source_idx ON reports (source_db_id);
